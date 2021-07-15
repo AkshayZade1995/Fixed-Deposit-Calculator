c8a90e2925d30a74462ba7a6b2ebfeaced5bc66e
Akshay <akshayzade.runtimesolutions@gmail.com>
2021-07-15 19:13:53 +0530

    logic complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  // State variables
+  var [fdamount, setFdAmount] = useState();
+  const [interest, setInterest] = useState();
+  const [period, setPeriod] = useState();
+  const [result, setResult] = useState();
+
+  // Function to calc amount
+  function calculateTotal() {
+    var quaterly = 3;
+    var tempAmount = 0;
+    var calcAmount = 0;
+
+    while(quaterly <= period) {
+			calcAmount = fdamount * interest/4/100;
+			tempAmount = fdamount + calcAmount;
+			fdamount = tempAmount;
+			quaterly = quaterly + 3;
+		}
+
+    setResult(Math.round(tempAmount));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fixed Deposit Calculator</h1>
+
+      <div>
+        <div>
+          <h3>Principal</h3>
+          <input
+            type="text"
+            value={fdamount}
+            onChange={e => setFdAmount(+e.target.value)}
+            placeholder="0"
+          />
+        </div>
+        
+        <div>
+          <h3>Rate Of Interest</h3>
+          <input
+            type="text"
+            value={interest}
+            onChange={e => setInterest(+e.target.value)}
+            placeholder="0"
+          />
+        </div>
+
+        <div>
+          <h3>Period of deposit (Months)</h3>
+          <input
+            type="text"
+            value={period}
+            onChange={e => setPeriod(+e.target.value)}
+            placeholder="0"
+          />
+        </div>
+      </div>
+
+      <div style={{paddingTop:'10px'}}>
+        <button onClick={calculateTotal}> Calculate </button>
+      </div>
+      
+
+      <h2>Maturity Amount : {result}</h2>
     </div>
   );
 }
